1d1dea6105cde182619bab609a7ab7886dc496bb
CJ <gnuser@163.com>
2022-07-02 12:00:05 +0800

    rename and format code

--- read-oracle.js
+++ read-oracle.js
@@ -1,12 +1,18 @@
 const { ethers } = require("ethers");
 const cronosOracleABI = require("./cronos-oracle-abi.js");
-const provider = new ethers.providers.JsonRpcProvider("https://rpc.vvs.finance");
+const provider = new ethers.providers.JsonRpcProvider(
+  "https://rpc.vvs.finance"
+);
 
 const cronosOracleAddress = "0xb3DF0a9582361db08EC100bd5d8CB70fa8579f4B";
 
 async function getLatestPrice() {
-    const oracleContract = new ethers.Contract(cronosOracleAddress, cronosOracleABI, provider);
-    /*
+  const oracleContract = new ethers.Contract(
+    cronosOracleAddress,
+    cronosOracleABI,
+    provider
+  );
+  /*
     let price = await oracleContract.latestAnswer();
     let humanReadablePrice = ethers.utils.formatUnits(price, 8);
     let timestamp = await oracleContract.latestTimestamp();
@@ -16,10 +22,10 @@ async function getLatestPrice() {
     console.log(humanReadablePrice, humanReadableTimestamp, humanReadableRound);
     */
 
-    let latestRoundData = await oracleContract.latestRoundData();
-    let price =  ethers.utils.formatUnits(latestRoundData.answer, 8);
-    let timestamp = ethers.utils.formatUnits(latestRoundData.updatedAt, 0) * 1000;
-    console.log(`[${new Date(timestamp).toLocaleString()}] ${price}`);
+  let latestRoundData = await oracleContract.latestRoundData();
+  let price = ethers.utils.formatUnits(latestRoundData.answer, 8);
+  let timestamp = ethers.utils.formatUnits(latestRoundData.updatedAt, 0) * 1000;
+  console.log(`[${new Date(timestamp).toLocaleString()}] ${price}`);
 }
 
 getLatestPrice();
