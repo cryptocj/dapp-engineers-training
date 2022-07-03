bb62d23a9a8aef0cc964ae070f62bd8a0dd41b16
CJ <gnuser@163.com>
2022-07-03 23:53:43 +0800

    Extract reportSingleCityTemperature

---
+++ constants.js
@@ -0,0 +1,26 @@
+const { ethers, Wallet } = require("ethers");
+
+const privateKey = process.env.PRIVATE_KEY;
+const weatherRecordAddress = "0x49354813d8BFCa86f778DfF4120ad80E4D96D74E";
+const multicallAddress = "0xd2e17686dD5642318e182179081854C7eB32fB56";
+const defaultGasPrice = ethers.utils.parseUnits("5000", "gwei");
+const provider = new ethers.providers.JsonRpcProvider(
+  "https://cronos-testnet-3.crypto.org:8545/"
+);
+const weatherRecordABI = require("./weather-record-abi.js");
+const weatherRecordContract = new ethers.Contract(
+  weatherRecordAddress,
+  weatherRecordABI,
+  provider
+);
+const wallet = new Wallet(privateKey, provider);
+
+module.exports = {
+  privateKey: privateKey,
+  weatherRecordAddress: weatherRecordAddress,
+  multicallAddress: multicallAddress,
+  defaultGasPrice: defaultGasPrice,
+  provider: provider,
+  weatherRecordContract: weatherRecordContract,
+  wallet: wallet,
+};
---
+++ report-single-city-temperature.js
@@ -0,0 +1,66 @@
+const {
+  getTemperatureWithDecimalByCity,
+  impossibleTemperature,
+} = require("./weather-source");
+const {
+  privateKey,
+  defaultGasPrice,
+  provider,
+  weatherRecordContract,
+  wallet,
+} = require("./constants");
+
+const {
+  getByteCodeByString,
+  encodeTemperatureWithSignBit,
+  outputTemperatureFromContract,
+} = require("./weather-contract-util");
+
+if (privateKey == undefined) {
+  console.log("please run with a private key like this");
+  console.log("  PRIVATE_KEY=xxxxx node write-weather.js");
+  process.exit(1);
+}
+
+async function reportSingleCityTemperature(cityName) {
+  const secondsSinceEpoch = Math.round(Date.now() / 1000);
+  let batchId = secondsSinceEpoch;
+
+  let temperatureWithDecimal = await getTemperatureWithDecimalByCity(cityName);
+  if (temperatureWithDecimal == impossibleTemperature) {
+    console.log("wrong temperature data");
+    return;
+  }
+  console.log(`report temperature ${temperatureWithDecimal} of ${cityName}`);
+  await reportWeatherRecord(batchId, cityName, temperatureWithDecimal);
+  await getWeatherRecord(batchId, cityName);
+}
+
+async function reportWeatherRecord(batchId, cityName, temperatureWithDecimal) {
+  let cityNameByteCode = getByteCodeByString(cityName);
+  const unsignedTx =
+    await weatherRecordContract.populateTransaction.reportWeather(
+      batchId,
+      cityNameByteCode,
+      encodeTemperatureWithSignBit(temperatureWithDecimal)
+    );
+  let estimatedGas = await provider.estimateGas(unsignedTx);
+  unsignedTx.gasLimit = estimatedGas;
+  unsignedTx.gasPrice = defaultGasPrice;
+  unsignedTx.nonce = await provider.getTransactionCount(wallet.address);
+  const signedTx = await wallet.signTransaction(unsignedTx);
+  const tx = await provider.sendTransaction(signedTx);
+  await provider.waitForTransaction(tx.hash);
+  console.log("confirmed tx id:", tx.hash);
+}
+
+async function getWeatherRecord(batchId, cityName) {
+  let cityNameByteCode = getByteCodeByString(cityName);
+  let record = await weatherRecordContract.getWeather(
+    batchId,
+    cityNameByteCode
+  );
+  outputTemperatureFromContract(batchId, cityName, record);
+}
+
+reportSingleCityTemperature("shenzhen");
