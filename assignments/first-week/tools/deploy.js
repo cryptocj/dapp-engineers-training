ebc90ba5b8fe580a6ffbd3be9002f227032ee361
CJ <gnuser@163.com>
2022-07-04 00:39:58 +0800

    Refactor move compile and deploy files

--- assignments/first-week/tools/deploy.js
+++ assignments/first-week/tools/deploy.js
@@ -4,6 +4,7 @@ const { Wallet } = require("ethers");
 
 ethers = require("ethers");
 fs = require("fs");
+require("../../../app");
 
 const provider = new ethers.providers.JsonRpcProvider(
   "https://cronos-testnet-3.crypto.org:8545/"
@@ -21,8 +22,12 @@ if (privateKey == undefined) {
 const wallet = new Wallet(privateKey, provider);
 
 //Read bin and abi file to object; names of the solcjs-generated files renamed
-bytecode = fs.readFileSync("./solc_output/Multicall2.bin").toString();
-abi = JSON.parse(fs.readFileSync("./solc_output/Multicall2.abi").toString());
+bytecode = fs
+  .readFileSync(`${__basedir}/solc_output/Multicall2.bin`)
+  .toString();
+abi = JSON.parse(
+  fs.readFileSync(`${__basedir}/solc_output/Multicall2.abi`).toString()
+);
 
 const account = wallet.connect(provider);
 
