521f5fa87eec1ca1644849bf9d2429663c8cfb5b
CJ <gnuser@163.com>
2022-07-03 19:59:16 +0800

    Add more impossible temperature cases

--- test/weather-source-test.js
+++ test/weather-source-test.js
@@ -13,8 +13,26 @@ describe("Weather source query and parse", function () {
 
   describe("parse weather source", function () {
     it("parseTemperature", function () {
-      let temperatureStrList = ["+27 °C", "+27.2 °C", "", "-27.2 °C", "-27 °C"];
-      let expectTemperature = [27, 27.2, impossibleTemperature, -27.2, -27];
+      let temperatureStrList = [
+        "+27 °C",
+        "+27.2 °C",
+        "",
+        "-27.2 °C",
+        "-27 °C",
+        "invalid weather",
+        "+10000 °C",
+        "-10000.2 °C",
+      ];
+      let expectTemperature = [
+        27,
+        27.2,
+        impossibleTemperature,
+        -27.2,
+        -27,
+        impossibleTemperature,
+        10000,
+        -10000.2,
+      ];
       for (let i = 0; i < temperatureStrList.length; i++) {
         expect(parseTemperature(temperatureStrList[i])).to.equal(
           expectTemperature[i]
